177d5981b3a498d3aa5bed8f7fbc9f94a4a691b4
Mike Job <mikejobrn@gmail.com>
2024-09-29 02:09:22 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/calculadora-dietetica-ionic',
+  base: '/calculadora-dietetica-ionic/',
   plugins: [
     react(),
     legacy()
